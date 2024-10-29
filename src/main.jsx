ddd4085ec06baddc1b1c6552ffcecba0019651c3
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2024-10-29 11:51:42 +0200

    commit7

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import ShoppingCartProvider from './context/ShoppingCartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <ShoppingCartProvider>
       <App/>
     </ShoppingCartProvider> 
-  </HashRouter>
+  </BrowserRouter>
 )
